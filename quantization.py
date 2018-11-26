bd05354600913aee6a51819f744aa2cc415f2f8d
nhatnt <trongnhat2312@gmail.com>
2018-11-26 15:00:25 +0700

    export to VN

--- quantization.py
+++ quantization.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 def main():
     f_max = 200
     fs = 20000
-    num_sample = 100
+    num_sample = 512
     muy = 255
 
     n = np.linspace(0, num_sample / fs, num_sample)
@@ -14,9 +14,19 @@ def main():
 
     y_abs = np.log(1 + muy * np.abs(signal)) / np.log(1 + muy)
     y_sign = np.sign(signal)
-    plt.figure(1)
-    plt.plot(n, y_abs)
+    y_encode = np.multiply(y_sign, y_abs)
 
+    y_decode = np.sign(y_encode) * 1 / muy * ((1 + muy) ** np.abs(y_encode) - 1)
+
+    plt.figure(1)
+    plt.subplot(311)
+    plt.plot(n, signal)
+
+    plt.subplot(312)
+    plt.plot(n, y_encode)
+    plt.subplot(313)
+
+    plt.plot(n, y_decode)
     plt.show()
 
 
